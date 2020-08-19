ca94f3273731be26ea555993d5389bfb6eea961d
Ulysse ARNAUD <mexanga@gmail.com>
2020-08-19 10:52:49 +0200

    MAJ Docker : tentative de resolution de composer

--- bin/clear.sh
+++ bin/clear.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-docker-compose down
+sudo docker-compose down
 
-if [ -d timer-ui ]; then rm -rf timer-ui; fi
-if [ -d timer-back ]; then rm -rf timer-back; fi
-if [ -f .env ]; then rm -f .env; fi
+if [ -d timer-ui ]; then sudo rm -rf timer-ui; fi
+if [ -d timer-back ]; then sudo rm -rf timer-back; fi
+if [ -f .env ]; then sudo rm -f .env; fi
--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
-if [ -d timer-ui ]; then rm -rf timer-ui; fi
+if [ -d timer-ui ]; then sudo rm -rf timer-ui; fi
 git clone https://github.com/ipssi-timer/timer-front.git timer-ui
-if [ -d timer-back ]; then rm -rf timer-back; fi
+if [ -d timer-back ]; then sudo rm -rf timer-back; fi
 git clone https://github.com/ipssi-timer/timer-back.git timer-back
-if [ -f .env ]; then rm -f .env; fi
+if [ -f .env ]; then sudo rm -f .env; fi
 cp -f .env.example .env
 
-sudo docker-compose up
+sudo chown -R 777 .
+
+sudo docker-compose up --build
